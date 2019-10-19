ba823ab7e3726e7870ac79f8d3f511163551a572
jclarke67 <jclarke@hamilton.edu>
2019-10-18 21:54:25 -0400

    Beginning data visualizer

---
+++ src/Components/Main.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+class Main extends Component {
+    constructor() {
+        super()
+        console.log('constructor')
+    }
+
+    render() {
+        console.log('render')
+        return< div>
+                <h1>"Data Visualizer"</h1>
+                {/* <Chart>"Data Visualizer"</Chart> */}
+                {/* <Agency></Agency> */}
+
+
+              </div>
+              
+    }
+}
+
+export default Main
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import Main from './Components/Main';
+import ReactDOM from 'react-dom';
+import './styles/stylesheet.css'
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
